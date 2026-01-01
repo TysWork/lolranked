c3f922279c9c0904da32689bc864f5318e61bf48
Ty <57588199+TysWork@users.noreply.github.com>
2026-01-01 13:48:52 -0500

    Update SQL script to use IF NOT EXISTS for tables
    Modified SQL script to ensure tables are created only if they do not already exist, and adjusted the structure of the 'order_items' and 'products' tables.

--- customer_profit.sql
+++ customer_profit.sql
@@ -1,31 +1,32 @@
-CREATE TABLE customers (
-  customer_id TEXT PRIMARY KEY,
-  customer_name TEXT
+CREATE TABLE IF NOT EXISTS customer(
+    customer_id TEXT PRIMARY KEY,
+    customer_name TEXT,
 );
 
-CREATE TABLE orders (
-  order_id TEXT PRIMARY KEY,
-  customer_id TEXT NOT NULL,
-  order_date TEXT,
-  ship_mode TEXT,
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+CREATE TABLE IF NOT EXISTS orders (
+    order_id TEXT PRIMARY KEY,
+    customer_id TEXT NOT NULL,
+    order_date TEXT,
+    ship_date TEXT,
+    ship_mode TEXT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
-CREATE TABLE order_items (
-  order_id TEXT NOT NULL,
-  product_id TEXT NOT NULL,
-  sales REAL,
-  profit REAL,
-  quantity INTEGER,
-  discount REAL,
-  PRIMARY KEY (order_id, product_id),
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (product_id) REFERENCES products(product_id)
+CREATE TABLE IF NOT EXISTS products (
+    product_id TEXT PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    category TEXT,
+    sub_category TEXT
 );
 
-CREATE TABLE products (
-  product_id TEXT PRIMARY KEY,
-  product_name TEXT,
-  category TEXT,
-  sub_category TEXT
+CREATE TABLE IF NOT EXISTS order_items (
+    row_id INTEGER PRIMARY KEY,
+    order_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    sales REAL,
+    quantity INTEGER,
+    discount REAL,
+    profit REAL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
